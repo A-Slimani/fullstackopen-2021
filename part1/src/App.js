ccdb147827ade8d00bc0c8ee243d4963c0b8bf3b
AbdullahSl98 <abdullahslimani98@gmail.com>
2021-06-12 20:42:37 +1000

    did exercise 1.1 not sure if correct

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,5 +1,23 @@
 import React from 'react'
 
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <p>{props.part} {props.exercise}</p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>The number of exercises {props.e1 + props.e2 + props.e3}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack Application Development'
   const part1 = 'Fundamentals of React'
@@ -11,19 +29,23 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercise={exercises1} />
+      <Content part={part2} exercise={exercises2} />
+      <Content part={part3} exercise={exercises3} />
+      <Total  e1={exercises1} e2={exercises2} e3={exercises3} />
     </div>
   )
 }
 
 export default App
+
+/*
+WHAT IT SHOULD LOOK LIKE
+
+<h1> Half Stack Application Development </h1>
+<p> Fundamentals of React 10 </p>
+<p> Using props to pass data 7 </p>
+<p> State of a component 14 </p>
+<p> Number of exercises 31 </p>
+*/
