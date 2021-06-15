3cca927812803ff4514736b13a162cd92960dbb1
AbdullahSl98 <abdullahslimani98@gmail.com>
2021-06-15 11:58:15 +1000

    completed unicafe

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,18 +1,40 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Statistic = ({ text, value, p }) => {
+  return (
+    <tr>
+      <td> {text} </td>
+      <td>
+        {value} {p}
+      </td>
+    </tr>
+  );
+};
+
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positive = (good / total) * 100;
 
+  if (total === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
   return (
-    <div>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {(good - bad) / total}</p>
-      <p>positive {(good / total) * 100} %</p>
-    </div>
+    <table>
+      <tbody>
+        <Statistic text="good" value={good} />
+        <Statistic text="neutral" value={neutral} />
+        <Statistic text="bad" value={bad} />
+        <Statistic text="total" value={total} />
+        <Statistic text="average" value={average} />
+        <Statistic text="positive" value={positive} p={"%"} />
+      </tbody>
+    </table>
   );
 };
 
@@ -21,7 +43,6 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-kkkk
   const Buttons = () => {
     return (
       <div>
