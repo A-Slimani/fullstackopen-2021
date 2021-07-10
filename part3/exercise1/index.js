7ff17cdb752057a1210ff52972bb3a4f1af402f0
AbdullahSl98 <abdullahslimani98@gmail.com>
2021-07-10 16:19:59 +1000

    working w heroku

--- part3/exercise1/index.js
+++ part3/exercise1/index.js
@@ -4,8 +4,12 @@
 // 	stored in the app variable
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors())
+
+
 
 const requestLogger = (request, response, text) => {
   console.log("Method: ", request.method);
@@ -15,7 +19,7 @@ const requestLogger = (request, response, text) => {
   next();
 };
 
-app.use(requestLogger)
+app.use(requestLogger);
 
 let notes = [
   { id: 1, content: "HTML is easy", date: "2019-05-30T17:30:31.098Z", important: true },
@@ -90,9 +94,9 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
