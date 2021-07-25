738b50834d5deb26e82530acaaf74351e986ecdc
AbdullahSl98 <abdullahslimani98@gmail.com>
2021-07-26 08:56:18 +1000

    completed testing node applications

--- part4/blogList/tests/palindrome.test.js
+++ part4/blogList/tests/palindrome.test.js
@@ -1,8 +1,7 @@
 const palindrome = require('../utils/for_testing').palindrome;
 
 test('palindrome of a', () => {
-  const result = palindrome('a');
-  expect(result).toBe('a');
+  expect(palindrome('a')).toBe('a');
 });
 
 test('palindrome of react', () => {
