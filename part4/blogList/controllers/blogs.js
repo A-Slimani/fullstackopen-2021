edf0cf927d94293e7a4dc2c595c66c9ad53e2df5
AbdullahSl98 <abdullahslimani98@gmail.com>
2021-08-05 11:29:29 +1000

    testing backend - supertest

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -1,18 +1,18 @@
 const blogRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-// blogRouter.get('/api/blogs', (req, res) => {
-//   Blog.find({}).then(blogs => {
-//     res.json(blogs);
-//   });
-// });
+blogRouter.get('/api/blogs', (req, res) => {
+  Blog.find({}).then(blogs => {
+    res.json(blogs);
+  });
+});
 
 blogRouter.get('/', async (req, res) => {
   const blogs = await Blog.find({});
   res.json(blogs);
 });
 
-blogRouter.post('/api/blogs', (req, res) => {
+blogRouter.post('/api/blogs', async (req, res) => {
   const blog = new Blog(req.body);
 
   try {
