0cb355e4af8606e737b0d0a691da80ce923925b0
AbdullahSl98 <abdullahslimani98@gmail.com>
2021-08-05 11:14:40 +1000

    testing backend - supertest

--- part4/blogList/tests/blog_api.test.js
+++ part4/blogList/tests/blog_api.test.js
@@ -1,58 +1,16 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+
 const api = supertest(app);
-const Blog = require('../models/blog');
-const initialBlog = [
-  {
-    title: 'first blog',
-    author: 'me',
-    url: 'newBlog.url',
-    likes: 22,
-  },
-];
 
-describe('testing the server', () => {
-  test('notes returned as json', async () => {
-    await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
-  });
-  test('there are three blogs', async () => {
-    const response = await api.get('/api/blogs');
-
-    expect(response.body).toHaveLength(3);
-  });
-  test('the first blog title is named title', async () => {
-    const response = await api.get('/api/blogs');
-
-    expect(response.body[0].title).toBe('title');
-  });
-  test('a valid blog can be added', async () => {
-    const newBlog = {
-      title: 'test-title',
-      author: 'aboud',
-      url: 'testBlog.url',
-      likes: 1,
-    };
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
-  });
-
-  const response = await api.get('/api/blogs');
-
-  const title = response.body.map(r => r.title);
-
-  expect(response.body).toHaveLength(initialNotes.length + 1);
-  expect(title).toContain('test-title');
+test('blogs returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
 });
 
-// closes the mongoose database connection
 afterAll(() => {
   mongoose.connection.close();
 });
