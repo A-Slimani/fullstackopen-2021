3253709e0c376421430f447a16aacad5d2fb1427
AbdullahSl98 <abdullahslimani98@gmail.com>
2021-08-05 12:38:35 +1000

    testing to backend - Initialising the database before tests

--- part4/blogList/tests/blog_api.test.js
+++ part4/blogList/tests/blog_api.test.js
@@ -1,8 +1,30 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
-
 const api = supertest(app);
+const Blog = require('../models/blog');
+const initialBlogs = [
+  {
+    title: 'first blog',
+    author: 'Abdullah',
+    url: 'newBlog.org',
+    likes: 22,
+  },
+  {
+    title: 'second one',
+    author: 'Slimani',
+    url: 'nsecond.org',
+    likes: 44,
+  }
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
 
 test('blogs returned as json', async () => {
   await api
@@ -10,6 +32,20 @@ test('blogs returned as json', async () => {
     .expect(200)
     .expect('Content-Type', /application\/json/);
 }, 100000);
+
+test('all blogs are returned', async () => {
+  const res = await api.get('/api/blogs');
+  expect(res.body).toHaveLength(initialBlogs.length);
+});
+
+test('a specific blog is within the returned blog', async () => {
+  const res = await api.get('/api/blogs');
+
+  const contents = res.body.map(r => r.content)
+  expect(contents).toContain(
+    'second one'
+  )
+});
 
 afterAll(() => {
   mongoose.connection.close();
